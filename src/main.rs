8803cc893e3f833a4c3fe24c18bffaf82f309e2e
Alex Gievsky <alex.gievsky@gmail.com>
2021-12-21 16:40:08 +0100

    Introduce /sleep

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,49 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
-use std::fs;
+use std::{
+	fs,
+	io::{Read, Write},
+	net::{TcpListener, TcpStream},
+	thread,
+	time::Duration,
+};
 
 fn main() {
-    // connecting to a port is caleld binding
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	// connecting to a port is caleld binding
+	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
+	for stream in listener.incoming() {
+		let mut stream = stream.unwrap();
 
-        handle_connection(&mut stream);
-    }
+		handle_connection(&mut stream);
+	}
 }
 
 fn handle_connection(stream: &mut TcpStream) {
-    let mut buffer = [0; 1024];
+	let mut buffer = [0; 1024];
 
-    // `read` keeps track of what data it returns internally; hence, we need mut
-    stream.read(&mut buffer).unwrap();
+	// `read` keeps track of what data it returns internally; hence, we need mut
+	stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\r";
+	let get = b"GET / HTTP/1.1\r\n";
+	let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-				let (status, file_name) = if buffer.starts_with(get) {
-					("HTTP/1.1 200 OK", "index.html")
-				} else {
-					("HTTP/1.1 404 NOT FOUND", "404.html")
-				};
+	let (status, file_name) = if buffer.starts_with(get) {
+		("HTTP/1.1 200 OK", "index.html")
+	} else if buffer.starts_with(sleep) {
+		thread::sleep(Duration::from_secs(5));
 
-				let contents = fs::read_to_string(file_name).unwrap();
-				let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status, contents.len(), contents);
+		("HTTP/1.1 200 OK", "index.html")
+	} else {
+		("HTTP/1.1 404 NOT FOUND", "404.html")
+	};
 
-				stream.write(response.as_bytes()).unwrap();
-				stream.flush().unwrap();
+	let contents = fs::read_to_string(file_name).unwrap();
+	let response = format!(
+		"{}\r\nContent-Length: {}\r\n\r\n{}",
+		status,
+		contents.len(),
+		contents
+	);
+
+	stream.write(response.as_bytes()).unwrap();
+	stream.flush().unwrap();
 }
